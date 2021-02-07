e7ee86bca8f70e52d4450bffb641c0cd53294985
ichcanziho <ichcanzihopi@gmail.com>
2021-02-06 19:47:48 -0600

    clean main

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-from core import GameEasy
-from core import GameNormal
-from core import GameHard
 from core import WindowMananger
 import tkinter
 
 
 def main():
-    game = GameNormal(lang="fr_French",
-                      category="colores",
-                      debug=False)
-
-    game.play_rounds(1)
-
-
-if __name__ == '__main__':
     root = tkinter.Tk()
     settings = WindowMananger(root)
     settings.mainloop()
 
+
+if __name__ == '__main__':
+    main()
 
 
 
